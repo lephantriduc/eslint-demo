ba8db5ca786febbeb711ef2de1cab29dd708cc71
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2026-05-18 12:06:29 +0700

    feat: Update config to use the new rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,23 +1,26 @@
 import js from "@eslint/js";
 import globals from "globals";
 import { defineConfig } from "eslint/config";
+import eslintPluginLocal from "./custom-rules/eslint-plugin.js";
 
 export default defineConfig([
   js.configs.recommended,
   {
     files: ["**/*.{js,mjs,cjs}"],
-    languageOptions: { 
-        sourceType: "module",
-        globals: {
-            ...globals.browser,
-            ...globals.node
-        }
+    languageOptions: {
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
     },
+    plugins: { local: eslintPluginLocal },
     rules: {
       "no-console": "error",
-      "semi": "error",
-      "eqeqeq": "error",
+      semi: "error",
+      eqeqeq: "error",
       "keyword-spacing": "error",
+      "local/enforce-file-comment": "error",
     },
-  }
+  },
 ]);
