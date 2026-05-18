d72a958688e2f75e763b0643cfd7488cde7d6c02
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2026-05-18 12:13:48 +0700

    chore: Delete initial demo file `index.js`

--- index.js
+++
@@ -1,9 +0,0 @@
-function showMessage() {
-  const message = "I like logs";
-    for (let i = 0; i < 5; i++) {
-    // eslint-disable-next-line no-console
-      console.log(message)
-  }
-}
-
-showMessage();
