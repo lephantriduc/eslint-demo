35fdaf2d33677bbda31ec79de9cb9d6c03a1a838
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2026-05-18 16:07:37 +0700

    refactor: Reformat local plugin file

--- custom-rules/eslint-plugin-local.js
+++ custom-rules/eslint-plugin-local.js
@@ -1,5 +1,10 @@
-const enforceFileCommentRule = require("./enforce-file-comment/enforce-file-comment")
-const requireDompurify = require('./require-dompurify/require-dompurify');
+const enforceFileCommentRule = require("./enforce-file-comment/enforce-file-comment");
+const requireDompurify = require("./require-dompurify/require-dompurify");
 
-const plugin = { rules: { "enforce-file-comment": enforceFileCommentRule, "require-dompurify": requireDompurify } };
+const plugin = {
+  rules: {
+    "enforce-file-comment": enforceFileCommentRule,
+    "require-dompurify": requireDompurify,
+  },
+};
 module.exports = plugin;
