979e25f2c5be76f13146cff0a8d4f09a1b94dad7
Le Phan Tri Duc <87744086+lephantriduc@users.noreply.github.com>
2026-05-18 16:32:41 +0700

    feat: Modify rule `require-dompurify`

--- custom-rules/require-dompurify/require-dompurify.js
+++ custom-rules/require-dompurify/require-dompurify.js
@@ -2,22 +2,36 @@ module.exports = {
   meta: {
     type: "problem",
     docs: {
-      description: "Require use of DOMPurify when assigning to innerHTML to prevent XSS vulnerabilities",
+      description:
+        "Require use of DOMPurify when assigning to innerHTML to prevent XSS vulnerabilities",
+      category: "Security",
+      recommened: true,
     },
+    messages: {
+      requireDOMPurifyComment: "Assigning data to 'innerHTML' without sanitation.",
+    },
+    fixable: "code",
+    schema: [],
+    languages: ["js/js"],
   },
   create(context) {
     return {
       "AssignmentExpression[left.property.name='innerHTML']"(node) {
-        
         const rightSide = context.sourceCode.getText(node.right);
-        
+
         if (!rightSide.includes("DOMPurify.sanitize")) {
           context.report({
             node,
-            message: "SECURITY WARNING: Assigning data to innerHTML without sanitization! Wrap the right-hand side with DOMPurify.sanitize().",
+            messageId: "requireDOMPurifyComment",
+            fix(fixer) {
+              return fixer.replaceText(
+                node.right,
+                `DOMPurify.sanitize(${rightSide})`,
+              );
+            },
           });
         }
-      }
+      },
     };
-  }
+  },
 };
